ff79a0f330fc5474242b6bc9877a7bbc63692810
Rajesh Rao <raorajesh7700@gmail.com>
2026-04-14 09:53:32 +0530

    added new project

---
+++ ratingreview.service/src/test/java/com/substring/foodie/ratingreview/ratingreview/service/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.substring.foodie.ratingreview.ratingreview.service;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
